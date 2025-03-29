160c458d588f73339bfe2c340d8b7e1a537cb60d
sambathreasmey <reasmeysambath@gmail.com>
2025-03-29 16:40:39 +0700

    1

--- django-project/saving_helper/saving_helper/settings.py
+++ django-project/saving_helper/saving_helper/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kp%l&c#ty1*2wsg*s8g2it4)p9=2bg6csjj(yub%r!nvgefge+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saving-helper-api.onrender.com']
 
 
 # Application definition
